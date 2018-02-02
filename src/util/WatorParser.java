1afe73a6b1b419b9c79dbfe8b482a7e1153ef7c0
August Ning <august.ning@duke.edu>
2018-02-01 22:36:30 -0500

    completed all the parsers for each simulation type

---
+++ src/util/WatorParser.java
@@ -0,0 +1,41 @@
+package util;
+
+
+/**
+ * @author August Ning
+ * Specific parser used for Wa-Tor World
+ *
+ */
+public class WatorParser extends XMLParser {
+	private int maxChronCount;
+	private int sharkStartingEnergy;
+	private int sharkMaxEnergy;
+	private int fishEnergy;
+	public WatorParser(String file) {
+		super(file);
+		this.parseGameConstants();
+		// TODO Auto-generated constructor stub
+	}
+	public void parseGameConstants() {
+		this.maxChronCount = (Integer.parseInt(this.getDoc().getElementsByTagName("max_chron_count").item(0).getTextContent()));
+		this.sharkStartingEnergy = (Integer.parseInt(this.getDoc().getElementsByTagName("shark_starting_energy").item(0).getTextContent()));
+		this.sharkMaxEnergy = Integer.parseInt(this.getDoc().getElementsByTagName("shark_max_energy").item(0).getTextContent());
+		this.fishEnergy = Integer.parseInt(this.getDoc().getElementsByTagName("fish_energy").item(0).getTextContent());
+	}
+	public int getMaxChronCount() {
+		return this.maxChronCount;
+	}
+	public int getSharkStartingEnergy() {
+		return this.sharkStartingEnergy;
+	}
+	public int getSharkMaxEnergy() {
+		return this.sharkMaxEnergy;
+	}
+	public int getFishEnergy() {
+		return this.fishEnergy;
+	}
+	public static void main (String[] args) {
+		WatorParser parser = new WatorParser("data\\XMLFiles\\wtest1.xml");
+		parser.printCells();
+	}
+}
